5dff6b07f492d9d273987a01111caa367a1e11e9
Stir <marketing@letscode.com.br>
2023-12-24 16:28:10 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, de jaqueta, óculos escuros, sem barba e com um fundo azul e roxo."
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, de camiseta preta, óculos de grau, barba e com um fundo amarelo."
+    )
+  }
+}
